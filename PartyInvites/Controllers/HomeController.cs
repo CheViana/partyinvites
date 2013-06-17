9b0adef0f060d8f6ee2dbeae04a8fde988db4f02
Raduga <raduga_jenya@yahoo.com>
2013-06-17 11:57:01 +0300

    sdd

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewData["Greeting"] = hour < 12 ? "Good morning" : "Good afternoon";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid == true)
+            {
+                //add code to mail the organizer
+                //
+                //
+                //
+                //
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                //there was a validation error - redisplay the form
+                return View();
+
+            }
+        }
+
+    }
+}
